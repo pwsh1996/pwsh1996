8cb1b23325605dcc4d28c08d97a70d9dbd951026
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-01-27 09:19:16 -0600

    Update Microsoft.Powershell_profile.ps1
    Added a alias for "cls" to also rewrite the banner at the top
    
    And added some notes on how to add

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,3 +1,31 @@
-# Verse of the day from Bible.com
+#
+# VERSE OF THE DAY BANNER
+#
 $verse = @(((wget https://www.bible.com/verse-of-the-day).content | where {$_ -match 'data-param-text='}).tostring().split('="') | where {$_ -match '[123]? \w+ \d+\:\d+-?\d*\: \w'})
 Write-Host $verse[0] -ForegroundColor Black -BackgroundColor white
+#
+# FUNCTIONS
+#
+function clear-host-verse {
+clear-host
+Write-Host $verse[0] -ForegroundColor Black -BackgroundColor white
+}
+#
+# ALIASES
+#
+set-alias -name CLS -Value clear-host-verse -Option AllScope # clears the screen and pastes the verse of the day at the top again
+
+
+###########################################################################
+# To Use:
+# In Windows PowerShell (not compatible with PowerShell Core) run
+# notepad.exe $profile
+# And paste the contents in, then save, you will need to restart PowerSehll
+#
+# If you run into problems adding it you may need to just check $profile
+# And make sure there is a path to where it is point, if not, create it
+#
+# You should also change the shortcut you're using to point to PowerShell
+# With the flag
+# -nologo 
+#
