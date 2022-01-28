91faa9965f83e57d46663a0023196f16afd61247
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-01-28 08:37:15 -0600

    Fixed some regex
    the expression '[123]? \w+ \d+\:\d+-?\d*\: \w' had the optional digit before the book (1 John, 2 Peter, etc) but didn't have the space between the digit and the book name, so nothing was returning, using '[123]? ?\w+ \d+\:\d+-?\d*\: \w' fixes it because there is now room for ?(one or none) spaces. 
    
    I think the regex is good, since the other day I pulled the Verse "1 John 2:15-16" and today pulled "Luke 6:45" which should cover all the different scenarios that should come up.

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,7 +1,7 @@
 #
 # VERSE OF THE DAY BANNER
 #
-$verse = @(((wget https://www.bible.com/verse-of-the-day).content | where {$_ -match 'data-param-text='}).tostring().split('="') | where {$_ -match '[123]? \w+ \d+\:\d+-?\d*\: \w'})
+$verse = @(((wget https://www.bible.com/verse-of-the-day).content | where {$_ -match 'data-param-text='}).tostring().split('="') | where {$_ -match '[123]? ?\w+ \d+\:\d+-?\d*\: \w'})
 Write-Host $verse[0] -ForegroundColor Black -BackgroundColor white
 #
 # FUNCTIONS
