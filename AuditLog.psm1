25c8b39fbe5b5bada888c47212a514bc5b87c705
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-04-08 16:51:40 -0500

    Update and rename Search-AuditLog.psm1 to AuditLog.psm1

--- AuditLog.psm1
+++ AuditLog.psm1
@@ -5,28 +5,91 @@
 # by: Jacob Petrie                                                                              #
 #################################################################################################
 
+# TO DO:
+#
+# [] create a better object as output for Search-AuditLog
+# [] create Backup-AuditLog
+# [] add switch -offline to Search-AuditLog to search the audit log created by Backup-AuditLog 
+# [] add better help to Search-AuditLog
+#
+
+function Backup-AuditLog {
+    <#
+        .SYNOPSIS
+            A tool to backup and splice the audit logs together for archiving purposes, to get past the 90 day retention period
+    #>
+    [CmdletBinding()]
+    param()
+    process {}
+}
+
+function StatusBar {
+    <#
+        .SYNOPSIS
+            To Simplify the output and make a cleaner looking progress bar
+        .PARAMETER progress
+            Takes the current progress in (for Math)
+        .PARAMETER text
+            Takes in text to output on the statusbar
+    #>
+    [CmdletBinding()]
+    param(
+        $progress,
+        $text
+    )
+
+    process {
+        $width = $Host.UI.RawUI.WindowSize.Width
+        $h = [math]::round($width*$progress)
+
+        if ($text.length -lt $width) {
+            $remaning = $width-($text.length)
+            $text += " "*$remaning
+        }
+        ClearStatusBar
+        for ($i = 0; $i -lt $width; $i++){
+            if ($i -le (($width-$h)-1)) {
+                Write-host "$($text[$i])" -BackgroundColor DarkGreen -NoNewline
+            }
+            else {
+                Write-host "$($text[$i])" -NoNewline
+            }
+        }
+    }
+}
+
+function ClearStatusBar {
+    [CmdletBinding()]
+    param()
+    process {
+        $width = $Host.UI.RawUI.WindowSize.Width
+        $delete = "`b" * $width
+        write-host $delete -NoNewline
+    }
+}
+
 function Search-AuditLog {
     <#
-    .SYNOPSIS
-        A wrapper for Search-UnifiedAuditLog that can be used for searching larger amounts of logs
-    .EXAMPLE
-        Search-AuditLog AzureActiveDirectory -start "03-22-2022" -end "03-24-2022" | ft  CreationDate, UserIds, Operations
-        
-        This will search for Audit Logs in AAD from March 22nd to the 24th
-    .PARAMETER record
-        Specifies the record type of the audit activities (also called operations) to search for. This property indicates the service or feature that an activity was triggered in. For a list of record types that you can use for this variable, https://docs.microsoft.com/en-us/office/office-365-management-api/office-365-management-activity-api-schema#auditlogrecordtype
-    .PARAMETER start
-        Specifies the start date of the range for the audit log search. The script will return records for audit activities that occurred within the specified date range.
-    .PARAMETER end
-        Specifies the end date of the range for the audit log search. The script will return records for audit activities that occurred within the specified date range.
-    .PARAMETER logFile
-        Specifies the name and location for the log file that contains information about the progress of the audit log search performed. The script writes UTC timestamps to the log file.
-    .PARAMETER outputFile
-        Specifies the name and location of the CSV file that contains the audit records returned.
-    .PARAMETER resultSize
-        Specifies the number of results returned each time the Search-UnifiedAuditLog cmdlet is called by the script (called a result set). The value of 5,000 is the maximum value supported by the cmdlet. Leave this value as-is.
-    .PARAMETER intervalMinutes
-        To help overcome the limit of 5000 records returned, this variable takes the data range you specified and slices it up into smaller time intervals. Now each interval, not the entire date range, is subject to the 5000 record output limit of the command.
+        .SYNOPSIS
+            A wrapper for Search-UnifiedAuditLog that can be used for searching larger amounts of logs
+        .EXAMPLE
+            Search-AuditLog AzureActiveDirectory -start "03-22-2022" -end "03-24-2022" | ft  CreationDate, UserIds, Operations
+            
+            This will search for Audit Logs in AAD from March 22nd to the 24th
+        .PARAMETER record
+            Specifies the record type of the audit activities (also called operations) to search for. This property indicates the service or feature that an activity was triggered in. For a list of record types that you can use for this variable, https://docs.microsoft.com/en-us/office/office-365-management-api/office-365-management-activity-api-schema#auditlogrecordtype
+        .PARAMETER start
+            Specifies the start date of the range for the audit log search. The script will return records for audit activities that occurred within the specified date range.
+        .PARAMETER end
+            Specifies the end date of the range for the audit log search. The script will return records for audit activities that occurred within the specified date range.
+        .PARAMETER logFile
+            Specifies the name and location for the log file that contains information about the progress of the audit log search performed. The script writes UTC timestamps to the log file.
+        .PARAMETER outputFile
+            Specifies the name and location of the JSON file that contains the audit records returned.
+        .PARAMETER resultSize
+            Specifies the number of results returned each time the Search-UnifiedAuditLog cmdlet is called by the script (called a result set). The value of 5,000 is the maximum value supported by the cmdlet. Leave this value as-is.
+        .PARAMETER intervalMinutes
+            To help overcome the limit of 5000 records returned, this variable takes the data range you specified and slices it up into smaller time intervals. Now each interval, not the entire date range, is subject to the 5000 record output limit of the command.
     #>
 
     [CmdletBinding()]
@@ -36,7 +99,7 @@ function Search-AuditLog {
         [DateTime]$start = [DateTime]::UtcNow.AddDays(-1),
         [DateTime]$end = [DateTime]::UtcNow,
         $logFile = "C:\AuditLogSearch\AuditLogSearchLog.txt",
-        $outputFile = "C:\AuditLogSearch\AuditLogRecords.csv",
+        $jsonfile = "C:\AuditLogSearch\AuditLogRecords.json",
         [validaterange(1, 5000)]
         $resultSize = 5000,
         $intervalMinutes = 60
@@ -44,7 +107,9 @@ function Search-AuditLog {
 
     process {
 
-        $outobject = @()
+        $totalhours = [math]::Round((($end) - $start).Totalhours)
+        $prog = 1
+        $jsondata = @()
 
         ###############################################################
         #  Checks to make sure they are connected to Exchange Online  #  
@@ -67,31 +132,28 @@ function Search-AuditLog {
         }
 
         Write-LogFile "BEGIN: Retrieving audit records between $($start) and $($end), RecordType=$record, PageSize=$resultSize."
-        Write-Host "Retrieving audit records for the date range between $($start) and $($end), RecordType=$record, ResultsSize=$resultSize"
+        StatusBar -progress $prog -text "Retrieving audit records for the date range between $($start) and $($end), RecordType=$record, ResultsSize=$resultSize"
 
         $totalCount = 0
         while ($true) {
             $currentEnd = $currentStart.AddMinutes($intervalMinutes)
-            if ($currentEnd -gt $end) {
-                $currentEnd = $end
-            }
+            if ($currentEnd -gt $end) { $currentEnd = $end }
 
-            if ($currentStart -eq $currentEnd) {
-                break
-            }
-
+            if ($currentStart -eq $currentEnd) { break }
             $sessionID = [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
+            $CurrentTeaLeft = [math]::Round((($end) - $currentstart).Totalhours)
+            $prog = [math]::round($CurrentTeaLeft/$totalhours,2)
             Write-LogFile "INFO: Retrieving audit records for activities performed between $($currentStart) and $($currentEnd)"
-            Write-Host "Retrieving audit records for activities performed between $($currentStart) and $($currentEnd)"
+            StatusBar -progress $prog -text "Retrieving audit records for activities performed between $($currentStart) and $($currentEnd)"
+            
             $currentCount = 0
 
             $sw = [Diagnostics.StopWatch]::StartNew()
             do {
                 $results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -RecordType $record -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize
-                
                 if (($results | Measure-Object).Count -ne 0) {
-                    $results | export-csv -Path $outputFile -Append -NoTypeInformation
-                    $outobject += $results
+                    $jsondata += $results.auditdata
+                    $results.auditdata | ConvertFrom-Json | Format-Table  CreationTime, UserId, Operation, ObjectId
                     $currentTotal = $results[0].ResultCount
                     $totalCount += $results.Count
                     $currentCount += $results.Count
@@ -100,8 +162,7 @@ function Search-AuditLog {
                     if ($currentTotal -eq $results[$results.Count - 1].ResultIndex) {
                         $message = "INFO: Successfully retrieved $($currentTotal) audit records for the current time range. Moving on!"
                         Write-LogFile $message
-                        Write-Host "Successfully retrieved $($currentTotal) audit records for the current time range. Moving on to the next interval." -foregroundColor Yellow
-                        ""
+                        StatusBar -progress $prog -text "Successfully retrieved $($currentTotal) audit records for the current time range. Moving on to the next interval."
                         break
                     }
                 }
@@ -113,8 +174,9 @@ function Search-AuditLog {
 
         Write-LogFile "END: Retrieving audit records between $($start) and $($end), RecordType=$record, PageSize=$resultSize, total count: $totalCount."
         Write-Host "Script complete! Finished retrieving audit records for the date range between $($start) and $($end). Total count: $totalCount" -foregroundColor Green
-        $outobject
+        $jsondata | out-file $jsonfile
     }
 }
 
 Export-ModuleMember -Function Search-AuditLog
+#Export-ModuleMember -Function Backup-AuditLog
