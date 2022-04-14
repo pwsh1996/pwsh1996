356985e6b17a8fe90428cad66a8f92d0812e8adf
Jacob Petrie <51274282+IZZY1996@users.noreply.github.com>
2022-04-14 17:31:17 -0500

    Update neofetch.ps1

--- neofetch.ps1
+++ neofetch.ps1
@@ -15,6 +15,10 @@ $meminfo = (Get-WmiObject win32_physicalmemory).capacity / 1024 /1024
 $consoleorvs = $host.name
 $osver = "$(((Get-WMIObject win32_operatingsystem).name).split("|")[0]) $((Get-WmiObject Win32_OperatingSystem).OSArchitecture)"
 
+Add-Type -AssemblyName System.Windows.Forms
+$screen = [System.Windows.Forms.Screen]::AllScreens[0].WorkingArea
+$resolution = "$($screen.Width)x$($screen.height)"
+
 ###############################
 #  Version Specific commands  # 
 ###############################
@@ -90,19 +94,20 @@ Write-Host '        ,.=:!!t3Z3z.,' -ForegroundColor red -NoNewline; write-host "
 Write-Host '       :tt:::tt333EE3' -ForegroundColor Red -NoNewline; Write-Host '                 ------------------------'
 write-host '       Et:::ztt33EEEL' -ForegroundColor Red -NoNewline; Write-Host ' @Ee.,      ..,' -ForegroundColor Green -NoNewline; Write-Host "  OS" -ForegroundColor Green -NoNewline; Write-Host ": $osver"
 Write-Host '      ;tt:::tt333EE7' -ForegroundColor Red -NoNewline; Write-Host ' ;EEEEEEttttt33#' -ForegroundColor Green -NoNewline; Write-Host "  Host" -ForegroundColor Green -NoNewline; Write-Host ": $hostmodel"
-Write-Host '     :Et:::zt333EEQ.' -ForegroundColor Red -NoNewline; Write-Host ' $EEEEEttttt33QL' -ForegroundColor Green -NoNewline; Write-Host "  Kernel" -ForegroundColor Green -NoNewline; Write-Host ": $kernelver"
-Write-Host '     it::::tt333EEF' -ForegroundColor Red -NoNewline; Write-Host ' @EEEEEEttttt33F' -ForegroundColor Green -NoNewline; Write-Host "   Uptime" -ForegroundColor Green -NoNewline; Write-Host ": $uptimeout"
-write-host '    ;3=*^```"*4EEV' -ForegroundColor Red -NoNewline; write-host ' :EEEEEEttttt33@.' -ForegroundColor Green -NoNewline; Write-Host "   Shell" -ForegroundColor Green -NoNewline; Write-Host ": $shelly"
-Write-Host '    ,.=::::!t=., ' -ForegroundColor Blue -NoNewline; Write-Host '`' -ForegroundColor Red -NoNewline; Write-Host ' @EEEEEEtttz33QF' -ForegroundColor Green -NoNewline; Write-Host "    Resolution" -ForegroundColor Green -NoNewline; Write-Host ': '
-write-host '   ;::::::::zt33)' -ForegroundColor Blue -NoNewline; write-host '   "4EEEtttji3P*' -ForegroundColor Green -NoNewline; Write-Host "     DE" -ForegroundColor Green -NoNewline; Write-Host ': '
-write-host '  :t::::::::tt33.' -ForegroundColor Blue -NoNewline; write-host ':Z3z..' -ForegroundColor Yellow -NoNewline; write-host '  ``' -ForegroundColor Green -NoNewline; Write-Host ' ,..g.' -ForegroundColor Yellow -NoNewline; Write-Host "     WM" -ForegroundColor Green -NoNewline; Write-Host ': '
-write-host '  i::::::::zt33F' -ForegroundColor Blue -NoNewline; write-host ' AEEEtttt::::ztF' -ForegroundColor Yellow -NoNewline; Write-Host "      WM Theme" -ForegroundColor Green -NoNewline; Write-Host ': '
-write-host ' ;:::::::::t33V' -ForegroundColor Blue -NoNewline; write-host ' ;EEEttttt::::t3' -ForegroundColor Yellow -NoNewline; Write-Host "       Terminal" -ForegroundColor Green -NoNewline; Write-Host ": $terminfo"
-write-host ' E::::::::zt33L' -ForegroundColor Blue -NoNewline; Write-Host ' @EEEtttt::::z3F' -ForegroundColor Yellow -NoNewline; Write-Host "       CPU" -ForegroundColor Green -NoNewline; Write-Host ": $cpuinfo"
-write-host '{3=*^```"*4E3)' -ForegroundColor Blue -NoNewline; write-host ' ;EEEtttt:::::tZ`' -ForegroundColor Yellow -NoNewline; Write-Host "       Memory" -ForegroundColor Green -NoNewline; Write-Host ": $meminfo"
-Write-Host '             `' -ForegroundColor Blue -NoNewline; Write-Host ' :EEEEtttt::::z7' -ForegroundColor Yellow
-write-host '                 "VEzjt:;;z>*`        ' -ForegroundColor Yellow -NoNewline
-write-host "   " -BackgroundColor Black -NoNewline
+Write-Host '     :Et:::zt333EEQ.' -ForegroundColor Red -NoNewline; Write-Host ' $EEEEEttttt33QL' -ForegroundColor Green -NoNewline
+Write-Host "  Kernel" -ForegroundColor Green -NoNewline; Write-Host ": $kernelver"
+Write-Host '     it::::tt333EEF' -ForegroundColor Red -NoNewline; Write-Host ' @EEEEEEttttt33F' -ForegroundColor Green -NoNewline
+Write-Host "   Uptime" -ForegroundColor Green -NoNewline; Write-Host ": $uptimeout"
+write-host '    ;3=*^```"*4EEV' -ForegroundColor Red -NoNewline; write-host ' :EEEEEEttttt33@.' -ForegroundColor Green -NoNewline
+Write-Host "   Shell" -ForegroundColor Green -NoNewline; Write-Host ": $shelly"
+Write-Host '    ,.=::::!t=., ' -ForegroundColor Blue -NoNewline; Write-Host '`' -ForegroundColor Red -NoNewline
+Write-Host ' @EEEEEEtttz33QF' -ForegroundColor Green -NoNewline
+Write-Host "    Resolution" -ForegroundColor Green -NoNewline; Write-Host ": $resolution"
+write-host '   ;::::::::zt33)' -ForegroundColor Blue -NoNewline; write-host '   "4EEEtttji3P*' -ForegroundColor Green -NoNewline; Write-Host "     Terminal" -ForegroundColor Green -NoNewline; Write-Host ": $terminfo"
+write-host '  :t::::::::tt33.' -ForegroundColor Blue -NoNewline; write-host ':Z3z..' -ForegroundColor Yellow -NoNewline; write-host '  ``' -ForegroundColor Green -NoNewline; Write-Host ' ,..g.' -ForegroundColor Yellow -NoNewline;  Write-Host "     CPU" -ForegroundColor Green -NoNewline; Write-Host ": $cpuinfo"
+write-host '  i::::::::zt33F' -ForegroundColor Blue -NoNewline; write-host ' AEEEtttt::::ztF' -ForegroundColor Yellow -nonewline; Write-Host "      Memory" -ForegroundColor Green -NoNewline; Write-Host ": $($meminfo)MB"
+write-host ' ;:::::::::t33V' -ForegroundColor Blue -NoNewline; write-host ' ;EEEttttt::::t3' -ForegroundColor Yellow
+write-host ' E::::::::zt33L' -ForegroundColor Blue -NoNewline; Write-Host ' @EEEtttt::::z3F' -ForegroundColor Yellow -NoNewline;write-host "       " -nonewline; write-host "   " -BackgroundColor Black -NoNewline
 write-host "   " -BackgroundColor darkRed -NoNewline
 write-host "   " -BackgroundColor darkGreen -NoNewline
 write-host "   " -BackgroundColor DarkYellow -NoNewline
@@ -110,8 +115,7 @@ write-host "   " -BackgroundColor darkBlue -NoNewline
 write-host "   " -BackgroundColor DarkMagenta -NoNewline
 write-host "   " -BackgroundColor DarkCyan -NoNewline
 write-host "   " -BackgroundColor Gray
-Write-Host '                                      ' -NoNewline
-write-host "   " -BackgroundColor DarkGray -NoNewline
+Write-Host '{3=*^```"*4E3)' -ForegroundColor Blue -NoNewline; write-host ' ;EEEtttt:::::tZ`' -ForegroundColor Yellow -NoNewline;write-host "       " -nonewline; write-host "   " -BackgroundColor DarkGray -NoNewline
 write-host "   " -BackgroundColor red -NoNewline
 write-host "   " -BackgroundColor Green -NoNewline
 write-host "   " -BackgroundColor Yellow -NoNewline
@@ -119,3 +123,6 @@ write-host "   " -BackgroundColor Blue -NoNewline
 write-host "   " -BackgroundColor Magenta -NoNewline
 write-host "   " -BackgroundColor Cyan -NoNewline
 write-host "   " -BackgroundColor White
+Write-Host '             `' -ForegroundColor Blue -NoNewline; Write-Host ' :EEEEtttt::::z7' -ForegroundColor Yellow
+write-host '                 "VEzjt:;;z>*`        ' -ForegroundColor Yellow
+Write-host ""
